1f51aa51f9419f648c1c64c47b44eda110bc36f4
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-12-24 23:33:41 +0530

    Updated Final Product

--- src/components/sessions/sessions.js
+++ src/components/sessions/sessions.js
@@ -32,48 +32,6 @@ const Session = ()=> {
             </div>
           </div>
         </div>
-        {/* SOCIAL PANEL HTML */}
-        <div className="social-panel-container">
-          <div className="social-panel">
-            <p>Created with <i className="fa fa-heart" /> by
-              <a target="_blank" href="https://florin-pop.com">Florin Pop</a></p>
-            <button className="close-btn"><i className="fas fa-times" /></button>
-            <h4>Get in touch on</h4>
-            <ul>
-              <li>
-                <a href="https://www.patreon.com/florinpop17" target="_blank">
-                  <i className="fab fa-discord" />
-                </a>
-              </li>
-              <li>
-                <a href="https://twitter.com/florinpop1705" target="_blank">
-                  <i className="fab fa-twitter" />
-                </a>
-              </li>
-              <li>
-                <a href="https://linkedin.com/in/florinpop17" target="_blank">
-                  <i className="fab fa-linkedin" />
-                </a>
-              </li>
-              <li>
-                <a href="https://facebook.com/florinpop17" target="_blank">
-                  <i className="fab fa-facebook" />
-                </a>
-              </li>
-              <li>
-                <a href="https://instagram.com/florinpop17" target="_blank">
-                  <i className="fab fa-instagram" />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <button className="floating-btn">
-          Get in Touch
-        </button>
-        <div className="floating-text">
-          Part of <a href="https://florin-pop.com/blog/2019/09/100-days-100-projects" target="_blank">#100Days100Projects</a>
-        </div>
       </div>
       </div>
  </div>)
